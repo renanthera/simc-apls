e57f49af8fc875a68d94caf68d182a01f13607ee
Kate Martin <51387586+renanthera@users.noreply.github.com>
2023-06-21 13:28:34 -0600

    update

--- run_sims.py
+++ run_sims.py
@@ -2,6 +2,7 @@
 import re
 import uuid
 import subprocess
+import json
 
 def parse_talent_strings(filename):
     data = []
@@ -13,12 +14,11 @@ def parse_talent_strings(filename):
                 data.append(match.group(1))
     return data
 
-def format_talent_strings(filename, uuid):
+def format_talent_strings(talent_strings, uuid):
     data = []
     a = 'profileset.\''
     b = '\'+=talents='
     suffix = '_' + str(uuid)
-    talent_strings = parse_talent_strings(filename)
 
     for talents in talent_strings:
         data.append(a + talents + suffix + b + talents + '\n')
@@ -39,9 +39,39 @@ def parse_apl(filename, uuid='', lines=['']):
         data.append(line + '\n')
     return data
 
+def parse_json(filename):
+    l = []
+    # select = ['name', 'mean', 'mean_error']
+    with open('cache/'+filename+'.json') as file:
+        data = json.load(file)
+    for entry in data['sim']['profilesets']['results']:
+        l.append(entry)
+        # l.append({k:entry[k] for k in select})
+    l.sort(key=lambda e: e['mean'])
+    return l
+
+def compute_maximums(data):
+    m = {}
+    for entry in data:
+        if m == {}:
+            m = entry
+        else:
+            for key in entry:
+                m[key] = max(m[key], entry[key])
+    return m
+
+def reduce_sims(uuid):
+    l = []
+    data = parse_json(str(uuid))
+    maximums = compute_maximums(data)
+    for entry in data:
+        if entry['mean'] >= maximums['mean'] - maximums['mean_error'] * 2:
+            l.append(entry['name'].split('_')[0])
+    return l
+
 def write_file(filename, data):
-    filename = str(filename)
     string = ''
+    filename = str(filename)
     for line in data:
         string += line
     with open('cache/' + filename + '.simc', 'w') as file:
@@ -49,15 +79,45 @@ def write_file(filename, data):
     with open('cache/uuid-index', 'a') as file:
         file.write(filename + '\n')
 
-suffixes = ['json2=cache/$(name).json', 'html=/dev/null', 'output=/dev/null', '']
-uuid = uuid.uuid4()
+def generate_sim(uuid):
+    suffixes = ['json2=cache/$(name).json', 'html=/dev/null', 'output=/dev/null', '']
 
-talent_strs = format_talent_strings('talents.simc', uuid)
-apl_header = parse_apl('brm.simc', uuid=uuid, lines=suffixes)
-apl = parse_apl('apl.simc')
+    talent_strings = parse_talent_strings('talents.simc')
+    talent_strings_formatted = format_talent_strings(talent_strings, uuid)
+    apl_header = parse_apl('brm.simc', uuid=uuid, lines=suffixes)
+    apl = parse_apl('apl.simc')
 
-f = apl_header + apl + talent_strs
+    f = apl_header + apl + talent_strings_formatted
 
-write_file(uuid, f)
+    write_file(uuid, f)
 
-# subprocess.run(['./simc', 'cache/' + str(uuid) + '.simc'])
+def run_sim(uuid):
+    subprocess.run(['./simc', 'cache/' + str(uuid) + '.simc'])
+
+def generate_child(configurations):
+
+
+# def prune_cache(age=current_simc_version(?)):
+# remove all files older than (age), remove all `.simc` without matching `.json`, clean up abandoned uuids from index
+# possibly instead use simc build data of a date
+
+# def improved(value)
+# display which configurations have improved over the past (value) generations
+# this possibly gets spun off elsewhere w/ helpers to better display what is improving/worsening graphically
+
+# uuid = uuid.uuid4()
+# generate_sim(uuid)
+# run_sim(uuid)
+
+
+d = reduce_sims('cac4151a-3121-433f-9c4d-10988a955b27')
+for k in d:
+    print(k)
+# maximums = compute_maximums(d)
+# ct = 1
+# for e in d:
+#     if e['mean'] >= maximums['mean'] - maximums['mean_error']*2:
+#         print(ct, e)
+#         ct += 1
+
+# print(maximums)
